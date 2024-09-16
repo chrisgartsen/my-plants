253660adc4a47d8cbd62ebb46a091e0b6f883d7b
Chris Gartsen.nl <chris@gartsen.nl>
2024-09-16 18:56:34 +0200

    Add navbar and styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup></script>
 
 <template>
-  <header>My Header</header>
+  <header>
+    <nav class="navbar">
+      <div class="container">
+        <div class="logo">My Plants</div>
+        <div class="nav-menu">
+          <ul>
+            <li><a href="Login.html" class="link-primary active">Log in</a></li>
+            <li><a href="Signup.html" class="link-primary">Sign up</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
   <main>My Content</main>
   <footer>My Footer</footer>
 </template>
