54d45853e9e4cc2ba4ea66fade37cf5b65abba6d
Chris Gartsen.nl <chris@gartsen.nl>
2024-09-19 19:07:55 +0200

    Setup vuelidate

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,8 @@ import LoginView from '../views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: LoginView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: LoginView
-    }
+    { path: '/', name: 'home', component: LoginView },
+    { path: '/login', name: 'login', component: LoginView }
   ]
 })
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,9 +4,41 @@
       <header class="card-header">
         <h3>Login</h3>
       </header>
-      <section class="card-content">The Form content</section>
+      <section class="card-content">
+        <form @submit.prevent="submitForm">
+          <input type="submit" value="Boem" />
+
+          {{ v$.email.$errors }}
+        </form>
+      </section>
 
       <footer class="card-footer">The card footer</footer>
     </div>
   </div>
 </template>
+
+<script setup>
+import { reactive } from 'vue'
+import useVuelidate from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+
+const credentials = reactive({
+  email: '',
+  password: ''
+})
+
+const rules = {
+  email: { required, email },
+  password: { required }
+}
+
+const v$ = useVuelidate(rules, credentials)
+
+const submitForm = async () => {
+  console.log('submitting form')
+  console.log(v$)
+  const r = await v$.value.$validate()
+  console.log(r)
+  console.log(v$.value.email.$errors[0].$message)
+}
+</script>
