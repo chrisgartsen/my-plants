34782ba1db1a513167d184adc84dc78b301ff97d
Chris Gartsen.nl <chris@gartsen.nl>
2024-09-20 19:42:19 +0200

    Add login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,26 +1,48 @@
 <template>
   <div id="login-page">
     <div class="card">
-      <header class="card-header">
-        <h3>Login</h3>
-      </header>
-      <section class="card-content">
-        <form @submit.prevent="submitForm">
-          <input type="submit" value="Boem" />
+      <form @submit.prevent="submitForm">
+        <header class="card-header">
+          <h3>Login</h3>
+        </header>
+        <section class="card-content">
+          <div class="form-component" :class="{ error: fieldHasError('email') }">
+            <input type="text" name="email" id="email" placeholder="" v-model="credentials.email" />
+            <label for="email">Email</label>
+            <div v-if="fieldHasError('email')" class="error-message">
+              {{ errorMessage('email') }}
+            </div>
+          </div>
+          <div class="form-component" :class="{ error: fieldHasError('password') }">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder=""
+              v-model="credentials.password"
+            />
+            <label for="password">Password</label>
+            <div v-if="fieldHasError('password')" class="error-message">
+              {{ errorMessage('password') }}
+            </div>
+          </div>
+        </section>
 
-          {{ v$.email.$errors }}
-        </form>
-      </section>
-
-      <footer class="card-footer">The card footer</footer>
+        <footer class="card-footer">
+          <div class="button-group">
+            <button class="btn-primary">Login</button>
+            <a class="btn-link">Forgot password?</a>
+          </div>
+        </footer>
+      </form>
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, computed } from 'vue'
 import useVuelidate from '@vuelidate/core'
-import { required, email } from '@vuelidate/validators'
+import { required, email, minLength } from '@vuelidate/validators'
 
 const credentials = reactive({
   email: '',
@@ -29,16 +51,28 @@ const credentials = reactive({
 
 const rules = {
   email: { required, email },
-  password: { required }
+  password: { required, minLength: minLength(6) }
 }
+
+const fieldHasError = computed(() => {
+  return (fieldName) => {
+    return v$.value[fieldName].$error
+  }
+})
+
+const errorMessage = computed(() => {
+  return (fieldName) => {
+    return v$.value[fieldName].$errors[0].$message
+  }
+})
 
 const v$ = useVuelidate(rules, credentials)
 
 const submitForm = async () => {
-  console.log('submitting form')
-  console.log(v$)
-  const r = await v$.value.$validate()
-  console.log(r)
-  console.log(v$.value.email.$errors[0].$message)
+  const valid = await v$.value.$validate()
+
+  if (valid) {
+    console.log('processing login')
+  }
 }
 </script>
