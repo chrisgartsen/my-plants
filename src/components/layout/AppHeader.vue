38ecfa0f0fec58d385604a654e29a201280e0e7e
Chris Gartsen.nl <chris@gartsen.nl>
2024-09-27 20:07:05 +0200

    Add mobile menu

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -17,20 +17,44 @@
             <li><a href="#" class="link-primary" @click="logout">Log out</a></li>
           </ul>
         </div>
+
+        <div class="hamburger" :class="{ active: showMobileMenu }" @click="toggleMobileMenu">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
       </div>
     </nav>
+
+    <div class="mobile-menu">
+      <ul>
+        <li><a href="#" class="link-primary active">Menu Item 1</a></li>
+        <li><a href="#" class="link-primary">Menu Item 2</a></li>
+        <li><a href="#" class="link-primary">Menu Item 3</a></li>
+        <li><a href="#" class="link-primary">Menu Item 4</a></li>
+      </ul>
+    </div>
   </header>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 import { useAuthStore } from '@/stores/auth'
 import { useRouter } from 'vue-router'
 
 const authStore = useAuthStore()
 const router = useRouter()
 
+const showMobileMenu = ref(false)
+
 const logout = async () => {
   await authStore.logout()
   router.push({ name: 'login' })
+}
+
+const toggleMobileMenu = () => {
+  showMobileMenu.value = !showMobileMenu.value
+  console.log(showMobileMenu.value)
 }
 </script>
