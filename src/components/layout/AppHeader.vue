cd20031fc8eb7ed090b1142693c5ab6e80fa5e41
Chris Gartsen.nl <chris@gartsen.nl>
2024-09-27 21:01:16 +0200

    Hide mobile menu on navigation

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -4,18 +4,7 @@
       <div class="container">
         <div class="logo">My Plants</div>
         <div class="nav-menu">
-          <ul v-if="!authStore.isAuthenticated">
-            <li><router-link :to="{ name: 'login' }" class="link-primary">Log in</router-link></li>
-            <li>
-              <router-link :to="{ name: 'signup' }" class="link-primary">Sign up</router-link>
-            </li>
-          </ul>
-          <ul v-if="authStore.isAuthenticated">
-            <li><a href="Signup.html" class="link-primary">Plants</a></li>
-            <li><a href="Signup.html" class="link-primary">Library</a></li>
-            <li><a href="Signup.html" class="link-primary">Account</a></li>
-            <li><a href="#" class="link-primary" @click="logout">Log out</a></li>
-          </ul>
+          <nav-menu @toggleMenu="toggleMobileMenu" />
         </div>
 
         <div class="hamburger" :class="{ active: showMobileMenu }" @click="toggleMobileMenu">
@@ -27,34 +16,18 @@
     </nav>
 
     <div class="mobile-menu" :class="{ active: showMobileMenu }">
-      <ul>
-        <li><a href="#" class="link-primary active">Menu Item 1</a></li>
-        <li><a href="#" class="link-primary">Menu Item 2</a></li>
-        <li><a href="#" class="link-primary">Menu Item 3</a></li>
-        <li><a href="#" class="link-primary">Menu Item 4</a></li>
-      </ul>
+      <nav-menu @toggleMenu="toggleMobileMenu" />
     </div>
   </header>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
-import { useAuthStore } from '@/stores/auth'
-import { useRouter } from 'vue-router'
-
-const authStore = useAuthStore()
-const router = useRouter()
+import NavMenu from '@/components/nav/NavMenu.vue'
 
 const showMobileMenu = ref(false)
 
-const logout = async () => {
-  await authStore.logout()
-  router.push({ name: 'login' })
-}
-
 const toggleMobileMenu = () => {
   showMobileMenu.value = !showMobileMenu.value
-  console.log(showMobileMenu.value)
 }
 </script>
---
+++ src/components/nav/NavMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul v-if="!authStore.isAuthenticated">
+      <li>
+        <router-link :to="{ name: 'login' }" class="link-primary" @click="navigate">
+          Log in
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'signup' }" class="link-primary" @click="navigate">
+          Sign up
+        </router-link>
+      </li>
+    </ul>
+    <ul v-if="authStore.isAuthenticated">
+      <li><a href="Signup.html" class="link-primary" @click="navigate">Plants</a></li>
+      <li><a href="Signup.html" class="link-primary">Library</a></li>
+      <li><a href="Signup.html" class="link-primary">Account</a></li>
+      <li><a href="#" class="link-primary" @click="logout">Log out</a></li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const emits = defineEmits(['toggleMenu'])
+
+const logout = async () => {
+  await authStore.logout()
+  router.push({ name: 'login' })
+}
+
+const navigate = () => {
+  emits('toggleMenu')
+}
+</script>
