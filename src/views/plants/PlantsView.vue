cb1cc6aa82a6c4aa04d9ee5aa9858a77e86823b6
Chris Gartsen.nl <chris@gartsen.nl>
2024-10-19 19:55:39 +0200

    Add plant cards

--- src/views/plants/PlantsView.vue
+++ src/views/plants/PlantsView.vue
@@ -4,7 +4,27 @@
   <div class="card-grid">
     <div v-for="plant in plants" :key="plant.name" class="card">
       <div class="card-body">
-        {{ plant.name }}
+        <div class="plant-image">
+          <div class="image-overlay">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-pen-to-square"></i>
+            <i class="fa-solid fa-trash"></i>
+          </div>
+          <img :src="plant.image" alt="" />
+        </div>
+        <div class="plant-description">
+          <h4>{{ plant.name }}</h4>
+
+          <div class="plant-codex">
+            <i class="fa-solid fa-cloud-sun"></i>
+
+            |
+
+            <i class="fa-solid fa-droplet"></i>
+            <i class="fa-solid fa-droplet"></i>
+            <i class="fa-solid fa-droplet"></i>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -14,11 +34,11 @@
 import { reactive } from 'vue'
 
 const plants = reactive([
-  { name: 'Plant 1' },
-  { name: 'Plant 2' },
-  { name: 'Plant 3' },
-  { name: 'Plant 4' },
-  { name: 'Plant 5' },
-  { name: 'Plant 6' }
+  { name: 'Anthurium', image: '/assets/images/plant-5.jpg' },
+  { name: 'Spathipylum', image: '/assets/images/plant-1.jpg' },
+  { name: 'Sansevieria', image: '/assets/images/plant-2.jpg' },
+  { name: 'Dracaena Marginata', image: '/assets/images/plant-1.jpg' },
+  { name: 'Plant 5', image: '/assets/images/plant-3.jpg' },
+  { name: 'Plant 6', image: '/assets/images/plant-5.jpg' }
 ])
 </script>
