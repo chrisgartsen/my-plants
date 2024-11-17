eb97a64db7c3f28a6ac7c80ec0fdd78c6ed6d762
Chris Gartsen.nl <chris@gartsen.nl>
2024-11-17 16:19:34 +0100

    Add wizard component

---
+++ src/components/plants/AddPlantStepOne.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="card-content" v-if="stepNumber == 1">
+    <div class="form-component">
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder=""
+        v-bind:value="name"
+        @input="updateName"
+      />
+      <label for="name">Name</label>
+    </div>
+
+    <div class="radio-buttons">
+      <span>Sunlight</span>
+      <div class="form-component">
+        <label for="light">
+          <input type="radio" name="sunlight" value="light" v-model="formData.sunlight" />
+          <i class="fa fa-sun"></i>Light
+        </label>
+      </div>
+      <div class="form-component">
+        <label for="light">
+          <input type="radio" name="sunlight" value="half-shade" v-model="formData.sunlight" />
+          <i class="fa fa-cloud-sun"></i>Half Shade
+        </label>
+      </div>
+      <div class="form-component">
+        <label for="light">
+          <input type="radio" name="sunlight" value="shade" v-model="formData.sunlight" />
+          <i class="fa fa-cloud"></i>Shade
+        </label>
+      </div>
+    </div>
+
+    <div class="select">
+      <span>Water</span>
+      <div class="form-component">
+        <select name="water" id="water">
+          <option>Please Select</option>
+          <option value="1">Once per month</option>
+          <option value="2">Once per week</option>
+          <option value="3">Twice per week</option>
+          <option value="4">Daily</option>
+        </select>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const emit = defineEmits(['updateName'])
+const props = defineProps(['stepNumber', 'name'])
+
+const formData = reactive({
+  name: '',
+  sunlight: '',
+  water: ''
+})
+
+const updateName = (e) => {
+  console.log('Updating Name', e.target.value)
+  emit('updateValue', e.target.value)
+}
+</script>
--- src/views/plants/AddPlantView.vue
+++ src/views/plants/AddPlantView.vue
@@ -4,63 +4,23 @@
       <div class="card-body">
         <div class="card-header">
           <h3>Add Plant</h3>
+          <h4>Step {{ stepNumber }}</h4>
         </div>
 
-        <section class="card-content" v-if="stepNumber == 1">
-          <h4>Step {{ stepNumber }}</h4>
-
-          <div class="form-component">
-            <input type="text" name="name" id="name" placeholder="" v-model="formData.name" />
-            <label for="name">Name</label>
-          </div>
-
-          <div class="radio-buttons">
-            <span>Sunlight</span>
-            <div class="form-component">
-              <label for="light">
-                <input type="radio" name="sunlight" value="light" v-model="formData.sunlight" />
-                <i class="fa fa-sun"></i>Light
-              </label>
-            </div>
-            <div class="form-component">
-              <label for="light">
-                <input
-                  type="radio"
-                  name="sunlight"
-                  value="half-shade"
-                  v-model="formData.sunlight"
-                />
-                <i class="fa fa-cloud-sun"></i>Half Shade
-              </label>
-            </div>
-            <div class="form-component">
-              <label for="light">
-                <input type="radio" name="sunlight" value="shade" v-model="formData.sunlight" />
-                <i class="fa fa-cloud"></i>Shade
-              </label>
-            </div>
-          </div>
-
-          <div class="select">
-            <span>Water</span>
-            <div class="form-component">
-              <select name="water" id="water">
-                <option>Please Select</option>
-                <option value="1">Once per month</option>
-                <option value="2">Once per week</option>
-                <option value="3">Twice per week</option>
-                <option value="4">Daily</option>
-              </select>
-            </div>
-          </div>
-        </section>
+        <add-plant-step-one
+          step-number="1"
+          v-if="stepNumber == 1"
+          :formData="formData"
+          @updateFormData="updateFormData"
+        />
 
         <footer class="card-footer">
           <div class="wizard-buttons">
             <button class="btn-secondary" :disabled="stepNumber == 1" @click="previous">
               Previous
             </button>
-            <button class="btn-primary" @click="next">Next</button>
+            <button class="btn-primary" @click="next" v-if="stepNumber < steps">Next</button>
+            <button class="btn-primary" @click="save" v-if="stepNumber == steps">Save</button>
           </div>
         </footer>
       </div>
@@ -71,18 +31,34 @@
 <script setup>
 import { reactive, ref } from 'vue'
 
+import AddPlantStepOne from '@/components/plants/AddPlantStepOne.vue'
+
 const stepNumber = ref(1)
+const steps = ref(4)
 
 const previous = () => {
-  stepNumber.value--
+  if (stepNumber.value > 0) {
+    stepNumber.value--
+  }
 }
 
 const next = () => {
-  stepNumber.value++
+  if (stepNumber.value < steps.value) {
+    stepNumber.value++
+  }
+}
+
+const save = () => {
+  console.log('Saving form')
 }
 
 const formData = reactive({
-  name: '',
-  sunlight: 'light'
+  name: 'kjkj',
+  light: 'shade',
+  water: ''
 })
+
+const updateFormData = (newVal) => {
+  console.log('Updating Formdata - Parent', newVal)
+}
 </script>
